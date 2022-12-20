05cbb411a1645b9a12d96dd11ab36662ff40ba15
Tolya <ivanov.anatoliu@list.ru>
2022-12-20 15:28:50 +0300

    overloading

--- Baza/LB/Functions/Overloading.cpp
+++ Baza/LB/Functions/Overloading.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
 #include "Overloading.h"
+
+Overloading::Overloading(int sec)
+{
+    seconds = sec;
+}
+
+
+
+
+void Overloading::base()
+{
+    Overloading c1(20);
+    Overloading c2(10);
+    Overloading c3 = c1 + c2;
+    c3.display();
+}
+
+void Overloading::add(int a, int b)
+{
+    std::cout << "sum = " << (a + b) << std::endl;
+}
+
+void Overloading::add(double a, double b)
+{
+    std::cout << "sum = " << (a + b) << std::endl;
+}
+
+void Overloading::display()
+{
+    std::cout << seconds << " seconds" << std::endl;
+
+}
+
+/*
+ * Здесь функция оператора не является частью класса Counter и определена вне его.
+ * Данная функция перегружает оператор сложения для типа Counter.
+ * Она является бинарной, поэтому принимает два параметра.
+ * В данном случае мы складываем два объекта Counter.
+ * Возвращает функция также объект Counter, который хранит общее количесто секунд.
+ * То есть по сути здесь операция сложения сводится к сложению секунд обоих объектов:
+ */
--- Baza/LB/Functions/Overloading.h
+++ Baza/LB/Functions/Overloading.h
@@ -3,5 +3,30 @@
 class Overloading
 {
 public:
+    int seconds;
+    void display();
+    void base();
+    Overloading(int sec);
+
+    Overloading& operator++ ()              // operator overloading prefix
+    {
+        seconds += 5;
+        return *this;
+    }
+
+    Overloading operator++ (int)            // operator overloading postfix
+    {
+        Overloading prev = *this;
+        ++*this;
+        return prev;
+    }
+
+    Overloading operator+(Overloading c1, Overloading c2)    // operator overloading
+    {
+        return Overloading(c1.seconds + c2.seconds);
+    }
+
+    void add(int a, int b);         // func overloading
+    void  add(double a, double b);  // func overloading
     
 };
